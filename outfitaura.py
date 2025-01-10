10313f5e93c8f038831f23b3a24424b78eac3487
brigid16 <165609469+brigid16@users.noreply.github.com>
2025-01-10 01:14:43 -0600

    Update outfitaura.py

--- outfitaura.py
+++ outfitaura.py
@@ -2,14 +2,18 @@
 ## Brigid O'Hara
 ## 9 Jan 25
 
-## variables
+# variables
 happy = ' blue, pink and yellow '
 calm = 'green, gray, and pink'
 sad = 'gray, blue, and red'
 
+# start
 print('Hey! Dress your mood with Outfit Aura. \n')
+
 name = input('What is your name: ')
-print('\n        Welcome, ' + name + '!! \n ')
+  print('\n        Welcome, ' + name + '!! \n ')
+
+# Q1 Aura Scale
 mood = int(input('Feel and resonate with your aura. \n '
                  'On a Scale of 1-10: \n  '
                  'How do you feel today? '))
@@ -26,9 +30,11 @@ elif mood <= 4:
 else:
         print(' \nUnavailiable, wear nothing? \n')
 
+# Q2 Change Aura Scale Input
 
 switch = int(input('Auras are meant to change. Try something different? ' ))
 
+##    COMMMENTSSS   ##
 ## while switch is
 ## def switch()
 ##def mood()
@@ -36,9 +42,6 @@ switch = int(input('Auras are meant to change. Try something different? ' ))
 ##print('Have a good day, ' + name + '! Stay Stylish')
 ## elif switch == 'yes':
         
-
-
-
 ##Add variables - practice pulling from a file
 ## FIX - ' less than 4 ' to ' less than or equal to 4 '
 ##Option to be given more or less than 3 colors
