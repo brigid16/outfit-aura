8ab20005446208e83d544d41bb498d7ce5963ead
brigid16 <165609469+brigid16@users.noreply.github.com>
2025-01-10 01:00:23 -0600

    basic practice

---
+++ outfitaura.py
@@ -0,0 +1,53 @@
+## Outfit Aura
+## Brigid O'Hara
+## 9 Jan 25
+
+## variables
+happy = ' blue, pink and yellow '
+calm = 'green, gray, and pink'
+sad = 'gray, blue, and red'
+
+print('Hey! Dress your mood with Outfit Aura. \n')
+name = input('What is your name: ')
+print('\n        Welcome, ' + name + '!! \n ')
+mood = int(input('Feel and resonate with your aura. \n '
+                 'On a Scale of 1-10: \n  '
+                 'How do you feel today? '))
+
+if mood >= 6:
+        print(' \n That is great, ' + name + ' ! Show others how bright the day can '
+              'be by wearing ' + happy + ' colors! \n')
+elif mood == 5:
+        print(' \n Not knowing how you feel is always okay! Wear ' + calm + ' to '
+              'stay smooth\n ')
+elif mood <= 4:
+        print(' \n Pretend dress up is soooo 3rd grade.'
+              ' Wear ' + sad + ' and settle for the blues. \n' )
+else:
+        print(' \nUnavailiable, wear nothing? \n')
+
+
+switch = int(input('Auras are meant to change. Try something different? ' ))
+
+## while switch is
+## def switch()
+##def mood()
+## if switch == 'no':
+##print('Have a good day, ' + name + '! Stay Stylish')
+## elif switch == 'yes':
+        
+
+
+
+##Add variables - practice pulling from a file
+## FIX - ' less than 4 ' to ' less than or equal to 4 '
+##Option to be given more or less than 3 colors
+##Add better descriptions
+##Different color combos per number/emotion - updates upon change in color quanity
+##Dress for your mood, y o n? Feeling like a 3, dress like an 8?
+## in statement '''
+                 
+                 
+
+
+
