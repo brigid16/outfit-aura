adf9a9dae44afc15a65ed62c123ad1db644468b7
brigid16 <165609469+brigid16@users.noreply.github.com>
2025-01-11 02:06:34 -0600

    I don't know how to properly update YET!

--- outfitaura.py
+++ outfitaura.py
@@ -2,55 +2,102 @@
 ## Brigid O'Hara
 ## 9 Jan 25
 
-# i think unnecessary variables
-happy = ' blue, pink and yellow '
+# Color variables
+happy = 'blue, pink and yellow'
+happy2 = 'pink, royal blue, and orange'
 calm = 'green, gray, and pink'
+calm2 = 'brown, scarlet, and green'
 sad = 'gray, blue, and red'
+sad2= 'black, indigo, and gray'
 
-# start
+
+# Start
 print('Hey! Dress your mood with Outfit Aura. \n')
 
 name = input('What is your name: ')
-  print('\n        Welcome, ' + name + '!! \n ')
+print('\n        Welcome, ' + name + '!! \n ')
 
-# Q1 Aura Scale
-mood = int(input('Feel and resonate with your aura. \n '
+
+# Q1 Aura Scale - 
+
+mood1 = int(input('Feel and resonate with your aura. \n '
                  'On a Scale of 1-10: \n  '
                  'How do you feel today? '))
 
-if mood >= 6:
-        print(' \n That is great, ' + name + ' ! Show others how bright the day can '
+if mood1 >= 6:
+        print('\nThat is great, ' + name + ' ! Show others how bright the day can '
               'be by wearing ' + happy + ' colors! \n')
-elif mood == 5:
-        print(' \n Not knowing how you feel is always okay! Wear ' + calm + ' to '
-              'stay smooth\n ')
-elif mood <= 4:
-        print(' \n Pretend dress up is soooo 3rd grade.'
-              ' Wear ' + sad + ' and settle for the blues. \n' )
+                
+elif mood1 == 5:
+        print('\nNot knowing how you feel is ALWAYS okay! Wear ' + calm +
+              ' to stay smooth!\n ')
+elif mood1 <= 4:
+        print('\nPretend dress up is soooo 3rd grade.'
+              'Wear ' + sad + ' and accept your gloom. \n' )
 else:
-        print(' \nUnavailiable, wear nothing? \n')
+        print('unavailiable.')
 
-# Q2 Change Aura Scale Input
 
-switch = int(input('Auras are meant to change. Try something different? ' ))
 
-##    COMMMENTSSS   ##
-## while switch is
-## def switch()
-##def mood()
-## if switch == 'no':
-##print('Have a good day, ' + name + '! Stay Stylish')
-## elif switch == 'yes':
+# Q2 Change Aura Scale Input or Color Output
+switch = input('Auras are meant to change. Try something different? ' )
+
+if switch == 'yes':
+        print('\nHow do you want to feel today? Scale of 1-10')
+        mood2 = int(input(' '))
+
+        if mood2 >= 6:
+                print('\nTotally, ' + name + '! Wear' + happy + 'colors! \n')
+                
+        elif mood2 == 5:
+                print('\n Wear ' + calm + 'and try your very best today.\n ')
+                
+        elif mood2 <= 4:
+                print('\nHow you feel is how you feel!'
+                      ' Wear ' + sad + ' and find comfort in these colors.\n' )
+  
+        else:
+                print(unavailable)
+
+                
+else:
         
+        newcolors = input('Glad you like how you feel! Change colors?' )
+
+        if newcolors == 'yes':
+                if mood1 >= 6:
+                        print('\nYou know what you like, ' + name + '! Wear ' + happy2 + ' colors instead! \n')
+                
+                elif mood1 == 5:
+                        print('\nWear ' + calm2 + 'to stay grounded.\n ')
+                        
+                elif mood1 <= 4:
+                        print('\nHow you feel is how you feel!'
+                                ' Wear ' + sad2 + ' colors. \n' )
+  
+                else:
+                         print('goodbye')
+                         
+                
+
+
+                
+              
+
+## simplify lines - if user only wants to change color (newcolors), can statement be sent back to mood1 input and conditions?
+                         ## no copied conditions
+
 ##Add variables - practice pulling from a file
-## FIX - ' less than 4 ' to ' less than or equal to 4 '
 ##Option to be given more or less than 3 colors
+## Option to switch out color
+                         ## happy.replace(1) 
 ##Add better descriptions
 ##Different color combos per number/emotion - updates upon change in color quanity
-##Dress for your mood, y o n? Feeling like a 3, dress like an 8?
-## in statement '''
+
                  
                  
+
+
 
 
 
